3d1e5365325c62088fc39a48beb2f4e9987fa9f8
faximon <vitorr.nascimento@hotmail.com>
2025-01-13 18:45:55 -0300

    Parse de uma lista de Strings para lista de Inteiros.
    Ignorando e retornando empty os valores que não se enquadram como inteiro através de um Optional.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        exerc1();
+
+    }
+
+    public static void exerc1() {
+        List<String> input = Arrays.asList("10", "abc", "20", "30x");
+        List<Integer> output = input.stream()
+                .map(str -> {
+                    try {
+                        return Optional.of(Integer.parseInt(str));
+                    } catch (NumberFormatException e) {
+                        return Optional.<Integer>empty();
+                    }
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        System.out.println(output);
     }
 }
+
